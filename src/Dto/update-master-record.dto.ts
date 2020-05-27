acb59bb30539afa17605602e59dc671b9a53d19c
Asmita Chavan <asmitachavan121@gmail.com>
2020-05-27 23:21:08 +0530

    completed e2e tests

---
+++ src/Dto/update-master-record.dto.ts
@@ -0,0 +1,78 @@
+import { IsIn, IsOptional, IsDate} from 'class-validator'
+import { coeStatusEnum, studentTypeEnum } from "../model/master-student.entity";
+import { genderEnum } from "../model/master-student.entity";
+import { Type } from "class-transformer";
+export class UpdateMasterRecordDto {
+
+    @IsOptional()
+    @IsIn([
+        coeStatusEnum.Approved,
+        coeStatusEnum.Studying
+    ])
+    coeStatus: coeStatusEnum
+
+    @IsOptional()
+    coeType: string
+
+    @IsOptional()
+    providerStudentID: number
+
+    @IsOptional()
+    firstName: string
+
+    @IsOptional()
+    familyName: string
+
+    @IsOptional()
+    @IsIn([
+        genderEnum.Female,
+        genderEnum.Male,
+        genderEnum.Other
+    ])
+    gender: genderEnum
+
+    @IsOptional()
+    @IsDate()
+    @Type(() => Date)
+    dateOfBirth: Date
+
+    @IsOptional()
+    nationality: string
+
+    @IsOptional()
+    courseName: string;
+
+    @IsOptional()
+    @Type(() => Date)
+    proposedStartDate: Date;
+
+    @IsOptional()
+    @Type(() => Date)
+    proposedEndDate: Date;
+
+    @IsOptional()
+    @Type(() => Date)
+    visaEffectiveDate: Date;
+
+    @IsOptional()
+    enrolmentComments: string;
+
+    @IsOptional()
+    locationName: string;
+
+    @IsOptional()
+    @IsIn([
+        studentTypeEnum.Commencing
+    ])
+    studentType: studentTypeEnum;
+
+    @IsOptional()
+    @Type(() => Date)
+    lastChangedDateTime: Date;
+    
+
+    @IsOptional()
+    @Type(() => Date)
+    createDateTime: Date;
+
+}
